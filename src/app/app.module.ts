bcef04aecf2e5db5fc03f5cb142705705785eb11
Youssf <youssfbr@gmail.com>
2019-11-03 22:12:39 -0300

    criacao componentes form e list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProdutoFormComponent } from './produtos/produto-form/produto-form.component';
+import { ProdutoListComponent } from './produtos/produto-list/produto-list.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutoFormComponent,
+    ProdutoListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FontAwesomeModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/produtos/produto-form/produto-form.component.ts
@@ -0,0 +1,36 @@
+import { Produto } from './../shared/produto';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-produto-form',
+  templateUrl: './produto-form.component.html',
+  styleUrls: ['./produto-form.component.css']
+})
+export class ProdutoFormComponent implements OnInit {
+  // produto: Produto;
+  title: string;
+  formProduto: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.title = 'Novo Produto';
+    // this.produto = new Produto();
+    this.createForm(new Produto());
+  }
+
+  createForm(produto: Produto) {
+    this.formProduto = this.fb.group({
+      nome: [produto.nome],
+      descricao: [produto.descricao],
+      preco: [produto.preco]
+    });
+  }
+
+  onSubmit() {
+    console.log(this.formProduto.value);
+
+    this.createForm(new Produto());
+  }
+}
